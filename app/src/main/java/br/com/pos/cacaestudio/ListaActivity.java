5cce7e934191341fbe9d063cf82b07b0a6d5e9c7
liviafonseca <36357045+liviafonseca@users.noreply.github.com>
2018-02-17 22:40:01 -0400

    incluindo tela de lista dos estúdios. View com menu lateral.

---
+++ app/src/main/java/br/com/pos/cacaestudio/ListaActivity.java
@@ -0,0 +1,115 @@
+package br.com.pos.cacaestudio;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.pos.cacaestudio.adapter.EstudiosAdapter;
+import br.com.pos.cacaestudio.modelo.Estudio;
+
+public class ListaActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Lista de Estúdios
+        Estudio estudio1 = new Estudio("Estúdio Tupira","Rua YYY, Santo Agostinho", "25");
+        Estudio estudio2 = new Estudio("Estúdio Sonora", "Rua XXX, Centro", "30");
+        List<Estudio> estudios = new ArrayList<Estudio>();
+        estudios.add(estudio1);
+        estudios.add(estudio2);
+
+        //Adapter da Lista
+        ListView listaEstudios = findViewById(R.id.lista_estudios);
+        EstudiosAdapter adapter = new EstudiosAdapter(this,estudios);
+        listaEstudios.setAdapter(adapter);
+
+        //View
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //para criar um menu de contexto
+        registerForContextMenu(listaEstudios);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        MenuItem itemAgendar = menu.add("Agendar");
+        MenuItem itemAvaliar = menu.add("Avaliar");
+
+        itemAgendar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(ListaActivity.this,"Cade a tela agendar?", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        itemAvaliar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(ListaActivity.this, AvaliarActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dados) {
+            //Intent intent = new Intent(ListaActivity.this,A)
+        } else if (id == R.id.nav_agendados) {
+            Intent intent = new Intent(ListaActivity.this, AgendadosActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_sobre) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/br/com/pos/cacaestudio/modelo/Estudio.java
@@ -0,0 +1,53 @@
+package br.com.pos.cacaestudio.modelo;
+
+/**
+ * Created by livia on 17/02/2018.
+ */
+
+public class Estudio {
+
+    private int id;
+    private String nome;
+    private String endereco;
+    private String preco;
+
+    public Estudio(String nome, String endereco, String preco) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+
+}
