82f9949e674582eceea7835a13b6882c8209e00f
Yukari Ota <yukari154@hotmail.com>
2018-02-18 19:11:07 -0400

    agora com a pasta drawable.

--- app/src/main/java/br/com/pos/cacaestudio/LoginActivity.java
+++ app/src/main/java/br/com/pos/cacaestudio/LoginActivity.java
@@ -1,11 +1,14 @@
 package br.com.pos.cacaestudio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button entrar;
@@ -25,5 +28,34 @@ public class LoginActivity extends AppCompatActivity {
         login = (EditText)findViewById(R.id.text_login);
         senha = (EditText)findViewById(R.id.text_senha);
 
-    }
-}
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Usuário: " + (String.valueOf(login.getText()))+
+                        "logado com sucesso"
+                        ,Toast.LENGTH_LONG).show();
+                Intent it = new Intent(LoginActivity.this, ListaActivity.class);
+                startActivity (it);
+            }
+
+        });
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, CadastrarActivity.class);
+                startActivity (it);
+            }
+
+        });
+        login_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Usuário Google logado com sucesso"
+                        ,Toast.LENGTH_LONG).show();
+                Intent it = new Intent(LoginActivity.this, ListaActivity.class);
+                startActivity (it);
+            }
+        });
+
+    }}
